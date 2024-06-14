f1e26afc47a4f860d007e3903ac755319849bf56
samuelngai <samuelngai@safp.gov.mo>
2024-06-14 11:52:21 +0800

    update

--- chroma-viewer/viewer.py
+++ chroma-viewer/viewer.py
@@ -6,14 +6,21 @@ import streamlit as st
 import argparse
 
 parser = argparse.ArgumentParser(description='Set the Chroma DB path to view collections')
-parser.add_argument('db')
+parser.add_argument('host', nargs='?', default='192.168.201.9')
+parser.add_argument('db', nargs='?', default='default_database')
 
 pd.set_option('display.max_columns', 4)
 
-def view_collections(dir):
+def view_collections(host, dir):
     st.markdown("### DB Path: %s" % dir)
 
-    client = chromadb.PersistentClient(path=dir)
+    # client = chromadb.PersistentClient(path=dir)
+    client = chromadb.HttpClient(
+        host=host,
+        settings=Settings(
+            allow_reset=True
+        )
+    )
 
     # This might take a while in the first execution if Chroma wants to download
     # the embedding transformer
@@ -36,8 +43,9 @@ def view_collections(dir):
 if __name__ == "__main__":
     try:
         args = parser.parse_args()
+        print("Host: %s" % args.host)
         print("Opening database: %s" % args.db)
-        view_collections(args.db)
+        view_collections(args.host, args.db)
     except:
         pass
 
